17c48c9c1ee9cfc16636a54f11b30f110bec3e3b
Adam Kariv <adam.kariv@gmail.com>
2019-09-30 12:58:53 +0300

    Cache docker builds

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build . -t akariv/google-chrome-in-a-box
+docker build . -t akariv/google-chrome-in-a-box --cache-from akariv/google-chrome-in-a-box
 docker push akariv/google-chrome-in-a-box
