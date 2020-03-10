bf6be93e6beaea35f452e2b843674323e7a722cb
Adam Kariv <adam.kariv@gmail.com>
2020-03-10 18:35:13 +0200

    Reorg, modify user-agent

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,5 @@
 # /etc/init.d/dbus start
 PORT=${1:-"9223"}
 DOWNLOADS_PORT=${2:-"9224"}
-xvfb-run -s "-screen 0 1200x900x24" /bin/bash -c "/app/startup.sh $PORT $DOWNLOADS_PORT"
+INITIAL=${3:-"https://data.gov.il"}
+xvfb-run -s "-screen 0 1200x900x24" /bin/bash -c "/app/startup.sh $PORT $DOWNLOADS_PORT $INITIAL"
--- startup.sh
+++ startup.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 echo "HI! LISTENING ON" ${1} ${2}
+echo "SCREENSHOT http://tzabar.obudget.org:${2}/screenshot"
+echo "INITIAL PAGE" ${3}
+USER_AGENT='Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36 datagov-external-client'
 mkdir /downloads
-socat tcp-listen:${1},fork tcp:localhost:9222 &
+(cd /downloads && python3 /app/server.py ${2}) &
 echo "1ST"
-(google-chrome --start-maximized --remote-debugging-port=9222 --no-sandbox --user-data-dir=/userdata/ http://127.0.0.1:9222) &
+(google-chrome --start-maximized --remote-debugging-port=9222 --no-sandbox --user-data-dir=/userdata/ --user-agent=$USER_AGENT ${3}) &
 sleep 3
 kill %2
 echo "2ND"
+socat tcp-listen:${1},fork tcp:localhost:19222 &
 python3 /app/fix_profile.py
-(google-chrome --start-maximized --remote-debugging-port=9222 --no-sandbox --user-data-dir=/userdata/ http://127.0.0.1:9222) &
+(google-chrome --start-maximized --remote-debugging-port=19222 --no-sandbox --user-data-dir=/userdata/ --user-agent=$USER_AGENT ${3}) &
 # # google-chrome-unstable --start-maximized --remote-debugging-port=9223 --disable-gpu http://127.0.0.1:9223 &
 sleep 3
 echo "STAT"
 curl 127.0.0.1:9222/json/list
 echo "WAIT"
-(cd /downloads && python3 /app/server.py ${2}) &
 sleep 14400
 # google-chrome-unstable --disable-gpu --no-sandbox --log-level=TRACE http://127.0.0.1:9223/
 # curl 127.0.0.1:9223/json/list
