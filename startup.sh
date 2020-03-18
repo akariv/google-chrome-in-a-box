e38234d3e3993fe829359182b650d4f6c0a6923e
Adam Kariv <adam.kariv@gmail.com>
2020-03-18 17:01:19 +0200

    Disable translate ui

--- startup.sh
+++ startup.sh
@@ -6,14 +6,14 @@ USER_AGENT='Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like
 mkdir /downloads
 (cd /downloads && python3 /app/server.py ${2}) &
 echo "1ST"
-(google-chrome --start-maximized --remote-debugging-port=9222 --no-sandbox --user-data-dir=/userdata/ --user-agent="$USER_AGENT" ${3}) &
+(google-chrome --start-maximized --remote-debugging-port=9222 --no-sandbox --user-data-dir=/userdata/ --disable-features=TranslateUI --user-agent="$USER_AGENT" ${3}) &
 sleep 30
 find /userdata/
 kill %2
 echo "2ND"
 socat tcp-listen:${1},fork tcp:localhost:19222 &
 python3 /app/fix_profile.py
-(google-chrome --start-maximized --remote-debugging-port=19222 --no-sandbox --user-data-dir=/userdata/ --user-agent="$USER_AGENT" ${3}) &
+(google-chrome --start-maximized --remote-debugging-port=19222 --no-sandbox --user-data-dir=/userdata/ --disable-features=TranslateUI --user-agent="$USER_AGENT" ${3}) &
 # # google-chrome-unstable --start-maximized --remote-debugging-port=9223 --disable-gpu http://127.0.0.1:9223 &
 sleep 3
 echo "STAT"
